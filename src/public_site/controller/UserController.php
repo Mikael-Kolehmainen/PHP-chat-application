b7204509a67b966600749c8baa3e3bedd936f8ae
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-01-17 21:34:54 +0200

    front-end for create user

--- src/public_site/controller/UserController.php
+++ src/public_site/controller/UserController.php
@@ -13,14 +13,25 @@ class UserController
     {
         echo "
         <section>
-            <article class='box'>
-                <h1>WELCOME BACK!</h1>
+            <article class='box create-user'>
+                <h1>WELCOME</h1>
                 <form action='' method='POST'>
-                    <input type='text' class='input-field' maxlength='25' onkeydown='return /[a-z0-9]/i.test(event.key)' placeholder='USERNAME' required>
+                    <label class='circle-file-input'>
+                        <input type='file' name='user-image' accept='png/jpg/jpeg/gif' required>
+                    </label>
+                    <input type='text' name='username' class='input-field' maxlength='25' onkeydown='return /[a-z0-9]/i.test(event.key)' placeholder='USERNAME' required>
                     <input type='password' class='input-field' maxlength='25' onkeydown='return /[a-z0-9]/i.test(event.key)' placeholder='PASSWORD' required>
-                    <input type='submit' class='btn' value='LOGIN'>
+                    <input type='password' name='pw' class='input-field' maxlength='25' onkeydown='return /[a-z0-9]/i.test(event.key)' placeholder='REPEAT PASSWORD' required>
+                    <div class='small-notice'>
+                        <p>Password must be at least:</p>
+                        <ul>
+                            <li id='pw-length-validation'>8 characters long</li>
+                            <li id='pw-number-letter-validation'>include numbers & letters</li>
+                        </ul>
+                    </div>
+                    <input type='submit' name='create' class='btn' value='CREATE'>
                 </form>
-                <a href='/index.php/user/log-in'>New? Click here</a>
+                <a href='/index.php/user/log-in'>Already a user? Click here</a>
             </article>
         </section>
         ";
@@ -30,12 +41,12 @@ class UserController
     {
         echo "
         <section>
-            <article class='box'>
+            <article class='box login'>
                 <h1>WELCOME BACK!</h1>
                 <form action='' method='POST'>
-                    <input type='text' class='input-field' maxlength='25' onkeydown='return /[a-z0-9]/i.test(event.key)' placeholder='USERNAME' required>
-                    <input type='password' class='input-field' maxlength='25' onkeydown='return /[a-z0-9]/i.test(event.key)' placeholder='PASSWORD' required>
-                    <input type='submit' class='btn' value='LOGIN'>
+                    <input type='text' name='username' class='input-field' maxlength='25' onkeydown='return /[a-z0-9]/i.test(event.key)' placeholder='USERNAME' required>
+                    <input type='password' name='pw' class='input-field' maxlength='25' onkeydown='return /[a-z0-9]/i.test(event.key)' placeholder='PASSWORD' required>
+                    <input type='submit' name='log-in' class='btn' value='LOGIN'>
                 </form>
                 <a href='/index.php/user/create'>New? Click here</a>
             </article>
